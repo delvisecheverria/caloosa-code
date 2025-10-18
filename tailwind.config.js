5963975e099f4f6d67868e1334ba03a6e762d910
Delvis Echeverria <41512567+delvisecheverria@users.noreply.github.com>
2025-10-18 18:46:08 -0400

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#FFD700",
+        amber: "#FFB700",
+        dark: "#0B0C10",
+      },
+    },
+  },
+  plugins: [],
+};
