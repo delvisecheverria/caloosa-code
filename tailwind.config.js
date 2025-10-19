5f7a8f27a67935d494685aa2d9c0f220afba336c
Delvis Echeverria <41512567+delvisecheverria@users.noreply.github.com>
2025-10-18 21:37:56 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        gold: "#FFD700",
-        amber: "#FFB700",
-        dark: "#0B0C10"
-      }
-    }
-  },
-  plugins: []
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
 };
